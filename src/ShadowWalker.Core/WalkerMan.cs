bc5e14161b86117b0584b2b7e3e439d8ca3892a9
Lu Rongkai <lurongkai@gmail.com>
2013-11-29 00:20:01 +0800

    init project.

---
+++ src/ShadowWalker.Core/WalkerMan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowWalker.Core
+{
+    public class WalkerMan
+    {
+        private WalkerMan() { }
+
+        public static WalkerMan InitializeWith(ISynchronizeStrategy synchronizeStrategy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TargetDescriptionBase> DoSynchronization() {
+            throw new NotImplementedException();
+        }
+    }
+}
